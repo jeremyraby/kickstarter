c7b36e30baeea3bbaae521fb2b496f8f7a65e7f4
jeremyraby <jeremy.raby10@gmail.com>
2023-03-23 21:16:34 -0500

    What project with a goal over ,000 USD, had the biggest Goal Completion % (Pledged / Goal)?

--- results.sql
+++ results.sql
@@ -18,3 +18,23 @@ FROM projects
 WHERE state = 'Successful';
 
 -- 133,851
+
+-- What project with a goal over $1,000 USD, had the biggest Goal Completion % (Pledged / Goal)? 
+
+SELECT
+    name,
+    goal,
+    pledged,
+    (pledged / goal) AS 'goal completion'
+FROM projects
+WHERE goal > 1000
+GROUP BY 1
+ORDER BY 4 DESC
+LIMIT 1;
+
+/*
+name, goal, pledged, goal completion
+VULFPECK /// The Beautiful Game, 1, 104278, 104278
+*/
+-- How much money was pledged?
+  -- $104,278
