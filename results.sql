40ed42c7432b9354ddb5ed373e443b629bc27483
jeremyraby <jeremy.raby10@gmail.com>
2023-03-26 14:07:14 -0500

    Fix typo on line 6

--- results.sql
+++ results.sql
@@ -3,14 +3,14 @@ SELECT
     category,
     COUNT(category) AS 'total_projects',
     COUNT(CASE WHEN state = 'Successful' THEN 1 END) AS 'total_successful',
-    ROUND((COUNT(CASE WHEN state = 'Successful' THEN 1 END) * 100.8) / COUNT(category), 2) AS 'percent_successful'
+    ROUND((COUNT(CASE WHEN state = 'Successful' THEN 1 END) * 100.0) / COUNT(category), 2) AS 'percent_successful'
 FROM projects
 GROUP BY 1
 ORDER BY 4 DESC
 LIMIT 1;
 /*
 category, total_projects, total_successful, percent_successful
-Dance, 3767, 2338, 62.56
+Dance, 3767, 2338, 62.07
 */
 
 /*
@@ -21,16 +21,6 @@ Dance, 2338
 
 -- What project with a goal over $1,000 USD, had the biggest Goal Completion % (Pledged / Goal)? 
 
-SELECT
-    name,
-    goal,
-    pledged,
-    (pledged / goal) AS 'goal completion'
-FROM projects
-WHERE goal > 1000
-GROUP BY 1
-ORDER BY 4 DESC
-LIMIT 1;
 
 /*
 name, goal, pledged, goal completion
